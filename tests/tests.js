c5aa29b086ef20f1d7fd66282005d9a9b6d5d299
emily <limeyb7@gmail.com>
2017-02-19 10:31:17 +0000

    removes relic of merge conflict

--- tests/tests.js
+++ tests/tests.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 QUnit.test( "hello test", function( assert ) {
   assert.ok( 1 == "1", "Passed!" );
 });
@@ -10,6 +9,3 @@ QUnit.test("getLanguages returns an array of languages with no duplicates", func
 QUnit.test("countStars returns an integer representing the total number of stars for all repos", function (assert) {
   assert.equal(countStars(JSON.parse(json)), 3);
 })
-=======
-// add your tests here!
->>>>>>> workshop
