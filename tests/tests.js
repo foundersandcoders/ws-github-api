36fd4559c62b08b7ca0bbc6c80aec021e9e8263b
jbarget <dear.justen@gmail.com>
2017-03-06 12:03:17 +0200

    adding tests and fixture for userResponse

--- tests/tests.js
+++ tests/tests.js
@@ -3,3 +3,15 @@ test( "hello test", function( assert ) {
 });
 
 // add your tests here using QUnit!
+
+test("renderPublicRepos test", function(assert){
+  // 1. test that 'github-user-repos' is blank/empty/""
+
+  var repoElement = document.getElementById('github-user-repos');
+  assert.equal(repoElement.textContent, "", "github-user-repos textContent is empty");
+  // 2. run the function
+
+  requestModule.renderPublicRepos(mockUserResponse);
+  // 3. test that its populated
+  assert.equal(repoElement.textContent, mockUserResponse.public_repos, "github-user-repos textContent is populated with " + mockUserResponse.public_repos  );
+})
