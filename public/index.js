ea1180333cc1362f9430e8f09cc5eb8f6218bcb3
heathercoraje <yhk7830@hotmail.com>
2018-01-10 23:51:08 +0900

    create request function with error handling callback

--- public/index.js
+++ public/index.js
@@ -1,16 +1,31 @@
 /* let's go! */
 var target = 'heathercoraje';
 
-function request(url) {
+function request(url, callback) {
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4 && xhr.status == 200){
-      var data = JSON.parse(xhr.responseText)
-      console.log(data);
+      callback(null, xhr.responseText);
+    }
+    else {
+      var errorMessage = xhr.responseText;
+      callback(errorMessage); // sending first argument (aka error)
     }
   };
   xhr.open("GET", url, true);
   xhr.send();
 }
 
-request('https://api.github.com/users/heathercoraje');
+
+function getRepoDetails (target) {
+  var url = 'https://api.github.com/users/' + target + '/repos';
+  request(url, function (error, result) {
+    if (error) {
+      console.log(error);
+      return;
+    } // if there is no error(null)
+    console.log('this is result', result);
+  });
+}
+
+getRepoDetails(target);
