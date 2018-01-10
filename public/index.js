b332c12c21e98d66d8631972f85782a4d08a3d0c
heathercoraje <yhk7830@hotmail.com>
2018-01-10 20:15:06 +0900

    setup create request function and add gitignore file

--- public/index.js
+++ public/index.js
@@ -1,1 +1,16 @@
 /* let's go! */
+var target = 'heathercoraje';
+
+function request(url) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200){
+      var data = JSON.parse(xhr.responseText)
+      console.log(data);
+    }
+  };
+  xhr.open("GET", url, true);
+  xhr.send();
+}
+
+request('https://api.github.com/users/heathercoraje');
