7e4fa8abc3677edddc4d5b211ae5ad372c49b716
DenisKent <kentdenis6@hotmail.co.uk>
2018-07-15 16:12:03 +0300

    part done

--- public/index.js
+++ public/index.js
@@ -1,1 +1,21 @@
 /* let's go! */
+var authToken = "23bc7a831fd94ac9693c1e2cc09aecb29de92b29";
+var name = "Solk10";
+var url = "https://api.github.com/users/" + name + "?access_token=" + authToken;
+
+function makeRequest(url, callback){
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function(){
+    if(xhr.readyState == 4 && xhr.status == 200){
+      var response = JSON.parse(xhr.responseText);
+      callback(response);
+    }
+  }
+  xhr.open("GET",url,true);
+  xhr.send();
+}
+
+makeRequest(url, function(userObj){
+  var avatar = document.getElementById("github-user-avatar");
+  avatar.src = userObj.avatar_url;
+});
